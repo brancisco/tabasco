b7505f9ff5dad4e58ca1ad83e9e3730a35dab301
Brandon Aguirre <brag4918@colorado.edu>
2019-05-19 17:14:58 -0600

    fix problems with pm2 and ports

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,16 @@ module.exports = {
     name: "tabasco",
     script: "./bin/www",
     watch: true,
-    error_file: "./.log/tabasco_error.log",
-    out_file: "./.log/tabasco_out.log",
-    pid_file: "./.log/tabasco_pid.log",
     env: {
       "PORT": 3000,
       "NODE_ENV": "development"
     },
     env_staging: {
-      "PORT": 3000,
+      "PORT": 8000,
       "NODE_ENV": "staging"
     },
     env_production: {
-      "PORT": 80,
+      "PORT": 8080,
       "NODE_ENV": "production"
     }
   }]
