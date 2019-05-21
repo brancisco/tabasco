635c5a3a63e7abef215b2720802f089971000668
Brandon Aguirre <brag4918@colorado.edu>
2019-05-21 02:03:22 -0600

    try adding absolute path in push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,12 +6,11 @@ if [[ "$2" != "production" ]]; then
   exit 1
 fi
 
-[ "$1" == "staging" ] && main_dir=~/staging || main_dir=~/www
+[ "$1" == "staging" ] && main_dir=/home/pi/staging || main_dir=/home/pi/www
 log_dir=.logs
 log_file=pull.log
 location=$main_dir/$log_dir/$log_file
 
-echo "$(date), $location" >> $location
-git reset --hard origin/$1 >> $location
+echo "$(date), $location, *$1*" >> $location
 git pull >> $location
 exit 0
