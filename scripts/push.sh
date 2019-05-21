be21a51816a176553a2b5bc38a915451c08e4a65
Brandon Aguirre <brag4918@colorado.edu>
2019-05-20 20:39:58 -0600

    update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,10 +6,10 @@ if [[ "$2" != "production" ]]; then
   exit 1
 fi
 
-[ "$1" == "staging" ] && cd ~/staging || cd ~/www
+[ "$1" == "staging" ] && main_dir=~/staging || main_dir=~/www
 log_dir=.logs
 log_file=pull.log
-location=$log_dir/$log_file
+location=$main_dir/$log_dir/$log_file
 
 echo "$(date), $location" >> $location
 git reset --hard origin/$1 >> $location
